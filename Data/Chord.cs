680639d213df065c0b314390c7ec0d71f5a79dfc
amayesingnathan <92682126+amayesingnathan@users.noreply.github.com>
2022-04-10 18:35:08 +0100

    Added ASP.NET/Blazor files

---
+++ Data/Chord.cs
@@ -0,0 +1,13 @@
+﻿namespace ScaleSourceWeb.Data
+{
+    public class Chord
+    {
+        public string? Strings { get; set; }
+        public string? Fingering { get; set; }
+        public string? ChordName { get; set; }
+        public string? EnharmonicChordName { get; set; }
+        public string? VoicingID { get; set; }
+        public string? Tones { get; set; }
+
+    }
+}
---
+++ Data/ChordFinderService.cs
@@ -0,0 +1,83 @@
+﻿using System.Net.Http.Headers;
+
+namespace ScaleSourceWeb.Data
+{
+    public class ChordFinderService
+    {
+
+        static HttpClient sClient = new HttpClient();
+        static bool sInitialised = false;
+        static DateTime? sLastChordTStamp = null;
+
+        public static void Init()
+        {
+            // Initialise HTTP Client on first call.
+            if (!sInitialised)
+            {
+                sClient.BaseAddress = new Uri("https://api.uberchord.com/v1/chords/");
+                sClient.DefaultRequestHeaders.Accept.Clear();
+                sClient.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+                sInitialised = true;
+            }
+        }
+        private static async Task<Chord?> GetChordAsync(string path)
+        {
+            Chord? chord = null;
+            HttpResponseMessage response = await sClient.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadAsAsync<List<Chord>>();
+                chord = result.FirstOrDefault();
+                if (chord is not null && chord.ChordName is not null)
+                    chord.ChordName = chord.ChordName.Replace(",", "");
+            }
+
+            sLastChordTStamp = DateTime.Now;
+            return chord;
+        }
+
+        public static async Task<Chord?> FindChord(string chordName)
+        {
+            // Initialise HTTP Client on first call.
+            if (!sInitialised)
+            {
+                sClient.BaseAddress = new Uri("https://api.uberchord.com/v1/chords/");
+                sClient.DefaultRequestHeaders.Accept.Clear();
+                sClient.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+                sInitialised = true;
+            }
+
+            if (sLastChordTStamp is null)
+                sLastChordTStamp = DateTime.Now;
+
+            try
+            {
+                return await GetChordAsync(chordName);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return null;
+        }
+
+        private static IEnumerable<Chord> PruneChords(IEnumerable<Chord?> Progression)
+        {
+            return Progression.Where(i => i != null);
+        }
+
+        public static IEnumerable<Chord> GetProgression(IEnumerable<string> ChordNames)
+        {
+            var ChordProgression = ChordNames.Select(name => FindChord(name).Result);
+
+            var FinalChordProgression = PruneChords(ChordProgression);
+
+            return FinalChordProgression;
+        }
+    }
+}
